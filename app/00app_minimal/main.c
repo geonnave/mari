d01077f5517badf9f58da29234f79eddc13db138
Geovane Fedrecheski <geonnave@gmail.com>
2025-03-12 14:07:09 +0100

    app/minimal: add minimal app to test OTA duration

---
+++ app/00app_minimal/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @ingroup     net_maclow
+ *
+ * @brief       Example on how to use the maclow driver
+ *
+ * @author Geovane Fedrecheski <geovane.fedrecheski@inria.fr>
+ *
+ * @copyright Inria, 2024
+ */
+#include <nrf.h>
+
+int main(void) {
+    while (1) {
+        __WFE();
+    }
+}
