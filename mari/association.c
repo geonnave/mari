7c21bd884f29fcc37bec99deef38a659c3e0e77d
Geovane Fedrecheski <geonnave@gmail.com>
2025-07-19 15:41:11 +0200

    assoc: set join request only when call start joining

--- mari/association.c
+++ mari/association.c
@@ -165,7 +165,6 @@ uint16_t mr_assoc_get_network_id(void) {
 void mr_assoc_node_handle_synced(void) {
     mr_assoc_set_state(JOIN_STATE_SYNCED);
     mr_assoc_node_reset_backoff();
-    mr_queue_set_join_request(mr_mac_get_synced_gateway());
     mr_sec_edhoc_set_state(EDHOC_M1_PENDING);
 }
 
@@ -178,6 +177,7 @@ bool mr_assoc_node_ready_to_join(void) {
 void mr_assoc_node_start_joining(void) {
     uint32_t now_ts                     = mr_timer_hf_now(MARI_TIMER_DEV);
     assoc_vars.join_response_timeout_ts = now_ts + MARI_JOINING_STATE_TIMEOUT;
+    mr_queue_set_join_request(mr_mac_get_synced_gateway());
     mr_assoc_set_state(JOIN_STATE_JOINING);
 }
 
