2a14953e6774e32c21397bd5a26a7d8f0f484099
Geovane Fedrecheski <geonnave@gmail.com>
2025-07-19 14:29:25 +0200

    app/sec: prepare m1 works

--- app/00sec_edhoc/main.c
+++ app/00sec_edhoc/main.c
@@ -24,30 +24,30 @@ edhoc_vars_t node_vars = { 0 };
 
 int main(void) {
     printf("Hello Mari Node\n");
-    // int8_t res = 0;
-    // uint32_t ts;
+    int8_t   res = 0;
+    uint32_t ts;
 
-    // mr_timer_hf_init(MARI_APP_TIMER_DEV);
+    mr_timer_hf_init(MARI_APP_TIMER_DEV);
 
-    // mr_sec_init();
+    mr_sec_init();
 
-    // while (1) {
-    //     ts = mr_timer_hf_now(MARI_APP_TIMER_DEV);
-    //     res = mr_sec_edhoc_init();
-    //     printf("init time: %u\n", mr_timer_hf_now(MARI_APP_TIMER_DEV)-ts);
-    //     if (res != 0) {
-    //         printf("Error sec: %d\n", res);
-    //     }
+    while (1) {
+        ts  = mr_timer_hf_now(MARI_APP_TIMER_DEV);
+        res = mr_sec_edhoc_init();
+        printf("init time: %u\n", mr_timer_hf_now(MARI_APP_TIMER_DEV) - ts);
+        if (res != 0) {
+            printf("Error sec: %d\n", res);
+        }
 
-    //     ts = mr_timer_hf_now(MARI_APP_TIMER_DEV);
-    //     res = mr_sec_edhoc_prepare_m1(node_vars.m1, &node_vars.m1_len);
-    //     printf("m1 time: %u\n", mr_timer_hf_now(MARI_APP_TIMER_DEV)-ts);
-    //     if (res != 0) {
-    //         printf("Error sec: %d\n", res);
-    //     }
+        ts  = mr_timer_hf_now(MARI_APP_TIMER_DEV);
+        res = mr_sec_edhoc_prepare_m1(node_vars.m1, &node_vars.m1_len);
+        printf("m1 time: %u\n", mr_timer_hf_now(MARI_APP_TIMER_DEV) - ts);
+        if (res != 0) {
+            printf("Error sec: %d\n", res);
+        }
 
-    //     __SEV();
-    //     __WFE();
-    //     __WFE();
-    // }
+        __SEV();
+        __WFE();
+        __WFE();
+    }
 }
